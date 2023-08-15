36b5e859d24554d60085035e43e9acb9a332c8fc
Gaby Marquez <gmarq006@gmail.com>
2023-08-14 22:52:07 -0400

    updates1

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// Logs window object using this
-console.log("this: ");
-console.log(this);
+document.getElementById("myForm").submit();
 
-// Logs the document object
-console.log("window.document: ");
-console.log(window.document);
 
-// Logs body of document
-console.log("document.body: ");
-console.log( document.body);
