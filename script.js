39dde5ca7f144a48d141019a41b500e4f37e4bd5
Gaby Marquez <gmarq006@gmail.com>
2023-08-15 23:31:32 -0400

    javascript

--- script.js
+++ script.js
@@ -1,3 +1,78 @@
-document.getElementById("myForm").submit();
+const questions = [
+    {
+        question: "What day is known as hump day?",
+        answers: [
+            { text: "Monday", correct: false },
+            { text: "Tuesday", correct: false },
+            { text: "Wednesday", correct: true },
+        ]
+    },
+    {
+        question: "What color is made after combining yellow and blue?",
+        answers: [
+            { text: "Purple", correct: false },
+            { text: "Grey", correct: false },
+            { text: "Green", correct: true },
+        ]
+    }
+]
 
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
 
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion() {
+    resetState()
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer)
+
+    });
+}
+
+function resetState() {
+    nextButton.style.display = "none";
+    while (answerButton.firstChild) {
+        answerButton.removeChild(answerButton.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+
+    });
+    nextButton.style.display = "block";
+}
+
+startQuiz();
